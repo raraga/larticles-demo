8819b5a9dcdb493589ef8082be635954150e891f
Renato Araga <admin@bkprecisioninstruments.com>
2019-05-23 17:03:11 -0700

    Making great progress; Starting the delete selection

---
+++ resources/js/components/Articles.vue
@@ -0,0 +1,71 @@
+<template>
+    <div>
+        <h2>Articles</h2>
+        <nav aria-label="Page navigation example">
+            <ul class="pagination">
+                <!-- Give this a class of disabled in the case that there are no previous pages available -->
+                <li
+                    v-bind:class="[{disabled: !pagination.prev_page_url}]"
+                    class="page-item"><a href="#" class="page-link" @click="fetchArticles(pagination.prev_page_url)">Previous</a>
+                </li>
+
+                <li class="page-item disabled"><a class="page-link text-dark" href="#">Page {{ pagination.current_page }} of {{ pagination.last_page }}</a></li>
+
+                <li
+                    v-bind:class="[{disabled: !pagination.next_page_url}]"
+                    class="page-item"><a href="#" class="page-link" @click="fetchArticles(pagination.next_page_url)">Next</a>
+                </li>
+            </ul>
+        </nav>
+        <div class="card card-body mb-2" v-for="article in articles" v-bind:key="article.id">
+            <h3>{{ article.title }}</h3>
+            <p>{{ article.body }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            articles: [],
+            article: {
+                id: '',
+                title: '',
+                body: ''
+            },
+            article_id: '',
+            pagination: {},
+            edit: false
+        };
+    },
+    // Think about created as the same thing as ComponentDidMount().
+    // It will run once the component is made.
+    created() {
+        this.fetchArticles();
+    },
+    methods: {
+        fetchArticles(page_url) {
+            let vm = this;
+            page_url = page_url || '/api/articles'
+            fetch(page_url)
+                .then(res => res.json())
+                .then(res => {
+                    // Pushing back up to our articles array within our data function.
+                    this.articles = res.data
+                    vm.makePagination(res.meta, res.links);
+                })
+                .catch(err => console.log(err));
+        },
+        makePagination(meta,links) {
+            let pagination = {
+                current_page: meta.current_page,
+                last_page: meta.last_page,
+                next_page_url: links.next,
+                prev_page_url: links.prev
+            }
+            this.pagination = pagination;
+        }
+    }
+}
+</script>
